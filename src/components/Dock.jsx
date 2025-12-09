0c01531f735f5b3f65453322ef11f8651ca50871
rzkylvx <rzkylvx@gmail.com>
2025-12-09 19:19:42 +0700

    feat: update react-dom to version 19.2.1 and enhance Dock component with GSAP animations and tooltips

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,8 +1,60 @@
 import { dockApps } from "#constants";
+import { Tooltip } from "react-tooltip";
 import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Dock = () => {
   const dockRef = useRef(null);
+
+  useGSAP(() => {
+    const dock = dockRef.current;
+    if (!dock) return;
+
+    const icons = dock.querySelectorAll(".dock-icon");
+
+    const animateIcons = (mouseX) => {
+      const { left } = dock.getBoundingClientRect();
+
+      icons.forEach((icon) => {
+        const { left: iconLeft, width } = icon.getBoundingClientRect();
+        const center = iconLeft - left + width / 2;
+        const distance = Math.abs(mouseX - center);
+
+        const intensity = Math.exp(-(distance ** 2.5) / 2000);
+
+        gsap.to(icon, {
+          scale: 1 + 0.25 * intensity,
+          y: -15 * intensity,
+          duration: 0.2,
+          ease: "power1.out",
+        });
+      });
+    };
+
+    const handleMouseMove = (e) => {
+      const { left } = dock.getBoundingClientRect();
+
+      animateIcons(e.clientX - left);
+    };
+
+    const resetIcons = () =>
+      icons.forEach((icon) =>
+        gsap.to(icon, {
+          scale: 1,
+          y: 0,
+          duration: 0.3,
+          ease: "power1.out",
+        })
+      );
+    dock.addEventListener("mousemove", handleMouseMove);
+    dock.addEventListener("mouseleave", resetIcons);
+
+    return () => {
+      dock.removeEventListener("mousemove", handleMouseMove);
+      dock.removeEventListener("mouseleave", resetIcons);
+    };
+  }, []);
 
   const toggleApp = (app) => {};
 
@@ -30,6 +82,8 @@ const Dock = () => {
             </button>
           </div>
         ))}
+
+        <Tooltip id="dock-tooltip" place="top" className="tooltip" />
       </div>
     </section>
   );
